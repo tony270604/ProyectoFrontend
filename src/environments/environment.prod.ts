cda0963193caf0310d0564491d78434460166a56
Antony <138824838+tony270604@users.noreply.github.com>
2024-12-14 10:13:43 -0500

    sin la barra final

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    backendUrl: 'https://proyectobackend-production-f9e2.up.railway.app/'
+    backendUrl: 'https://proyectobackend-production-f9e2.up.railway.app'
   };
